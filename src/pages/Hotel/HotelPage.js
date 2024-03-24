e11c32954ea9153a735f84a156cc7337039ff77a
Piotr Brauer <brauer10@gmail.com>
2024-03-24 22:12:33 +0100

    simple hotelPage was created

--- src/pages/Hotel/HotelPage.js
+++ src/pages/Hotel/HotelPage.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import LoadingIcon from "../../components/UI/LoadingIcon/LoadingIcon";
 
 function HotelPage (props) {
-    return (
-        <p>To będzie strona hotelu</p>
-    )
+
+    const params = useParams();
+    const [hotel, setHotel] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const fetchHotel = () => {
+        setHotel({id: 3,
+            name: "Apartment Modern",
+            city: "Paris",
+            rating: 9.5,
+            image: ''})
+            setLoading(false)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetchHotel();
+        }, 500);
+    }, []);
+
+    if (loading) return <LoadingIcon />
+    return <p>Hotel: {hotel.name}</p>
 }
 
 export default HotelPage;
