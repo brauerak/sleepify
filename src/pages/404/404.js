203eb54bdbf8e8c2e04bac90364649df7185d1fa
Piotr Brauer <brauer10@gmail.com>
2024-03-27 15:54:30 +0100

    404 page was created

---
+++ src/pages/404/404.js
@@ -0,0 +1,10 @@
+function NotFound(props) {
+    return (
+        <div className="text-center m-5">
+            <h2>404</h2>
+            <p>Not found</p>
+        </div>
+    );
+}
+
+export default NotFound;
