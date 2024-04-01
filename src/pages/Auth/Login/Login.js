d86375ff09acdfad30eb0ebff3cb3454fc68dc45
Piotr Brauer <brauer10@gmail.com>
2024-04-01 22:56:41 +0200

    Login component was created

---
+++ src/pages/Auth/Login/Login.js
@@ -0,0 +1,19 @@
+function Login (props) {
+    return (
+        <>
+            <div class="m-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+            </div>
+            <div class="m-3">
+                <label for="inputPassword5" class="form-label">Password</label>
+                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" />
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
