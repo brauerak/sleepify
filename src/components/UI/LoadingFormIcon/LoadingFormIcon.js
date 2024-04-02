541a6e65e043ced6a04a548bdbcec49dc1ada65d
Piotr Brauer <brauer10@gmail.com>
2024-04-02 18:00:42 +0200

    LoadingFormIcon component  created

---
+++ src/components/UI/LoadingFormIcon/LoadingFormIcon.js
@@ -0,0 +1,10 @@
+function LoadingFormIcon() {
+    return (
+        <div className="spinner-border m-3" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+    );
+  }
+  
+  export default LoadingFormIcon;
+  
