0dd64a74c4dffa93a13a80327b65b970f84a8818
Piotr Brauer <brauer10@gmail.com>
2024-04-02 17:59:35 +0200

    form in Login and Accdetails modified

--- src/pages/Account/AccountDetails/AccountDetails.js
+++ src/pages/Account/AccountDetails/AccountDetails.js
@@ -1,17 +1,98 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import LoadingFormIcon from '../../../components/UI/LoadingFormIcon/LoadingFormIcon';
+import validatePassword from '../../../helpers/validation/validationPassword';
+import validateEmail from '../../../helpers/validation/validationEmail';
+import useAuth from '../../../hooks/useAuth';
+
 function AccountDetails (props) {
+
+    const [email, setEmail] = useState('brauer@brauer.com');
+    const [password, setPassword] = useState('');
+    const [auth, setAuth] = useAuth();
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({
+        email: '',
+        passsword: ''
+    })
+    const navigate = useNavigate();
+
+    const submit = (e) => {
+        e.preventDefault();
+        setLoading(true)
+        setTimeout(() => {
+            /// validation
+
+            /// save details
+            
+        setLoading(false)
+        }, 1000)
+    }
+
+    useEffect(() => {
+        if(validateEmail(email)) {
+            setErrors({...errors, email: ''})
+        } else {
+            setErrors({...errors, email: 'Invalid email'})
+        }
+    },[email])
+    
+    useEffect(() => {
+        if(validatePassword(password) || !password) {
+            setErrors({...errors, passsword: ''})
+        } else {
+            setErrors({...errors, passsword: 'Invalid password'})
+        }
+    },[password])
+
+    //odfiltrować tablice objektu z elementów nieprawdziwych. (zostają same prawdziwe) Jeśli nie będzie zadnego błędu to zmienna ta wyniesie 0 czyli false
+    const buttonDisabled = Object.values(errors).filter(x => x).length
+   
+     
+
     return (
         <>
-            <div class="m-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="name@example.com" />
-            </div>
-            <div class="m-3">
-                <label for="inputPassword5" class="form-label">Password</label>
-                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" />
-                <div id="passwordHelpBlock" class="form-text">
-                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+            <form onSubmit={submit}>
+                <div class="m-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input
+                        className={`form-control ${errors.email
+                            ? 'is-invalid' 
+                            : 'is-valid'}`}
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        class="form-control" 
+                        placeholder="name@example.com"
+                    />
+                    <div className='invalid-feedback'> {errors.email}</div>
+                    <div className='valid-feedback'> OK! </div>
                 </div>
-            </div>
+                <div class="m-3">
+                    <label for="inputPassword5" class="form-label">Password</label>
+                    <input 
+                        className={`form-control ${errors.passsword 
+                            ? 'is-invalid' 
+                            : ''}`}
+                        type="password"
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        id="inputPassword5" 
+                        class="form-control" 
+                        aria-describedby="passwordHelpBlock" 
+                    />
+                    <div className='invalid-feedback'> {errors.passsword}</div>
+                    <div className='valid-feedback'> OK! </div>
+                    <div id="passwordHelpBlock" class="form-text">
+                        Your password must be 8-20 characters long, contain letters, numbers and special characters and must not contain spaces or emoji.
+                    </div>
+                </div>
+                {loading
+                    ? <LoadingFormIcon />
+                    : <button className="btn btn-secondary m-3" disabled={buttonDisabled}>Save</button>
+                    // I could make this statement above as a component becasue i will use it in every form, but now this is more readable for me. Maybe I will do it later. Then it is importent to provide 'loading' as props
+                }
+            </form>
         </>
     )
 }
--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,17 +1,70 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LoadingFormIcon from "../../../components/UI/LoadingFormIcon/LoadingFormIcon";
+import useAuth from "../../../hooks/useAuth";
+
 function Login (props) {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [auth, setAuth] = useAuth();
+    const [loading, setLoading] = useState(false);
+    const [valid, setValid] = useState(null)
+    const navigate = useNavigate();
+
+    const submit = (e) => {
+        e.preventDefault();
+        
+        setLoading(true)
+        setTimeout(() => {
+            /// login
+            if (true) {
+                setAuth(true);
+                navigate('/')
+            } else {
+                setValid(false)
+                setPassword('')
+            }
+        setLoading(false)
+        }, 1000)
+    }
+
     return (
         <>
-            <div class="m-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="name@example.com" />
-            </div>
-            <div class="m-3">
-                <label for="inputPassword5" class="form-label">Password</label>
-                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" />
-                <div id="passwordHelpBlock" class="form-text">
-                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+            {valid === false 
+                ? (<div className="alert alert-danger">Email or password not correct.</div>) 
+                : null }
+            <form onSubmit={submit}>
+                <div class="m-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input 
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        class="form-control" 
+                        placeholder="name@example.com" 
+                    />
                 </div>
-            </div>
+                <div class="m-3">
+                    <label for="inputPassword5" class="form-label">Password</label>
+                    <input 
+                        type="password"
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        id="inputPassword5" 
+                        class="form-control" 
+                        aria-describedby="passwordHelpBlock" 
+                    />
+                    <div id="passwordHelpBlock" class="form-text">
+                        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                    </div>
+                </div>
+                {loading
+                    ? <LoadingFormIcon />
+                    : <button className="btn btn-secondary m-3">Log in</button>
+                    // I could make this statement above as a component becasue i will use it in every form, but now this is more readable for me. Maybe I will do it later. Then it is importent to provide 'loading' as props
+                }
+            </form>
         </>
     )
 }
