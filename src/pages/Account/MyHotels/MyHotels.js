4b34700945490eccfe8ca438fcbac47e1c446d88
Piotr Brauer <brauer10@gmail.com>
2024-04-05 18:08:32 +0200

    MyHotels modified

--- src/pages/Account/MyHotels/MyHotels.js
+++ src/pages/Account/MyHotels/MyHotels.js
@@ -1,7 +1,11 @@
+import { Link, useLocation, useMatch } from "react-router-dom"
+
 function MyHotels (props) {
+
     return (
         <>
-        MyHotels
+            <div className="text-start m-3"> No hotels yet. </div>
+            <Link to={"/account/hotels/addhotel"} className='btn btn-outline-secondary m-2'>Add hotel</Link>
         </>
     )
 }
